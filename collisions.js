292aa980f2b49a5baeadace9a9741bffc17ecfe2
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2019-07-30 22:13:26 -0400

    Add files via upload

--- collisions.js
+++ collisions.js
@@ -1,3 +1,11 @@
+function headOnCollision(m1, v1, m2, v2) {
+  let v4 = (2*m1/(m1+m2))*v1 - ((m1-m2)/(m1+m2))*v2
+  let v3 = ((m1-m2)/(m1+m2))*v1 + (2*m2/(m1+m2))*v2
+  return {
+    'v3': v3,
+    'v4': v4
+  };
+}
 
 function proj12(x1_x, x1_y, x2_x, x2_y) {
   var obj = {
@@ -12,15 +20,6 @@ function proj12(x1_x, x1_y, x2_x, x2_y) {
   return obj;
 }
 
-function headOnCollision(m1, v1, m2, v2) {
-  let v4 = (2*m1/(m1+m2))*v1 - ((m1-m2)/(m1+m2))*v2
-  let v3 = ((m1-m2)/(m1+m2))*v1 + (2*m2/(m1+m2))*v2
-  return {
-    'v3': v3,
-    'v4': v4
-  };
-}
-
 function ortho12(x1_x, x1_y, x2_x, x2_y) {
   var obj1 = proj12(x1_x, x1_y, x2_x, x2_y);
   var obj2 = {};
@@ -29,65 +28,57 @@ function ortho12(x1_x, x1_y, x2_x, x2_y) {
   return obj2;
 }
 
-// needs 2 objects, each eith x, y, vx, vy, r, m
+// needs 2 objects, each with x, y, vx, vy, r, m
 function recalcMomentumAfterCollision(b1, b2) {
 	
-	let d = dist(b1.x, b1.y, b2.x, b2.y);
-	let rho = b1.r + b2.r;
+  let d = dist(b1.x, b1.y, b2.x, b2.y);
+  let rho = b1.r + b2.r;
 	
-	if (d > rho) {
+  if (d > rho) {	
+    // do nothing
+    return;
+  } else {
+
+    // 1. get the normal vector, N, that represents the line of collision
+    let N = {
+			'x': b1.x - b2.x,
+			'y': b1.y - b2.y,
+			'mag': function() {
+			  return Math.sqrt(this.x*this.x + this.y*this.y);
+		  }
+		}
+
+    // 1b. reposition b1 so that it is exactly rho units away from b2 in the direction of the line of collision, N
+    b1.x = b2.x + (N.x/N.mag())*rho;
+    b1.y = b2.y + (N.y/N.mag())*rho;
+    
+    // animating the line of collision
+    stroke(255, 0, 0);
+    strokeWeight(2);
+    line(b1.x, b1.y, b2.x, b2.y);
+		
+    // 2. get the projections of v1 and v2 onto the normal
 	
-		// do nothing
-		return;
-
-	}	else {
-
-		// 1. step 1: get the normal vector [done]
-
-		let normalX = b1.x - b2.x;
-		let normalY = b1.y - b2.y;
-		let mag = Math.sqrt(normalX*normalX + normalY*normalY)
+    // each returns an object
+    let projV1onN = proj12(b1.vx, b1.vy, N.x, N.y);
+    let projV2onN = proj12(b2.vx, b2.vy, N.x, N.y);
 		
-		stroke(255, 0, 0);
-		strokeWeight(2);
-		line(b1.x, b1.y, b2.x, b2.y);
-		
-		// 1b. position both circles so that they are no longer overlapping
-
-		b1.x = b2.x + (normalX/mag)*rho;
-		b1.y = b2.y + (normalY/mag)*rho;
-		
-		// obviously, it's totally arbitrary that I'm positioning b1 rho units away from b2, but it works
-		
-		
-		// 2. get the projections of v1 and v2 onto the normal [done]
-		
-			// this is an object with x and y
-			let projV1onN = proj12(b1.vx, b1.vy, normalX, normalY);
-			
-			// this is an object with x and y
-			let projV2onN = proj12(b2.vx, b2.vy, normalX, normalY);
-		
-		// 3. calculate the head on collision return values: i need a better algorithm
+		// 3. calculate the head on collision - break it up into x- and y-collisions since this is alone N
 
 		// in: m1, v1x, v1y, m2, v2x, v2y (basically, mass + 2 vectors in)
-
 		// out: v3x, v3y, v4x, v4y (2 vectors out)
 		
-		let v3_normalX = headOnCollision(b1.m, projV1onN.x, b2.m, projV2onN.x).v3;
-		let v4_normalX = headOnCollision(b1.m, projV1onN.x, b2.m, projV2onN.x).v4;
+    let v3_normalX = headOnCollision(b1.m, projV1onN.x, b2.m, projV2onN.x).v3;
+    let v4_normalX = headOnCollision(b1.m, projV1onN.x, b2.m, projV2onN.x).v4;
 		
-		let v3_normalY = headOnCollision(b1.m, projV1onN.y, b2.m, projV2onN.y).v3;
-		let v4_normalY = headOnCollision(b1.m, projV1onN.y, b2.m, projV2onN.y).v4;
+    let v3_normalY = headOnCollision(b1.m, projV1onN.y, b2.m, projV2onN.y).v3;
+    let v4_normalY = headOnCollision(b1.m, projV1onN.y, b2.m, projV2onN.y).v4;
 		
-
-		// 4. calc. orthogonal components [done]
+    // 4. calculate the orthogonal components
 		
-			// this is an object with x and y
-			let orthoV1onN = ortho12(b1.vx, b1.vy, normalX, normalY);
-			
-			// this is an object with x and y
-			let orthoV2onN = ortho12(b2.vx, b2.vy, normalX, normalY);
+    // each returns an object
+    let orthoV1onN = ortho12(b1.vx, b1.vy, N.x, N.y);
+    let orthoV2onN = ortho12(b2.vx, b2.vy, N.x, N.y);
 		
 		// 5. combine results from step 3 and 4
 		
@@ -102,6 +93,6 @@ function recalcMomentumAfterCollision(b1, b2) {
 		
 		b2.vx = v4x;
 		b2.vy = v4y;
-		
+  
 	}
 }
